d8958805298382a215aeb995f2ae349b15e3fbc5
Selva Ganesh M <selvadev2k@gmail.com>
2023-07-12 13:22:58 +0530

    rtk get all movies

--- src/redux/slices/movie/movie.er.ts
+++ src/redux/slices/movie/movie.er.ts
@@ -1,13 +1,47 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
-import api from "../../../config/api";
+import api, { TResponse } from "../../../config/api";
+import ensureError from "../../../utils/ensureError";
+import { apiKey } from "../../../config/envConfig";
+
+interface IMovieSrcData {
+  src: string;
+}
+
+export interface IMovie {
+  Title: string;
+  Year: number;
+  imdbID: string;
+  Type: string;
+  Poster: string;
+}
+
+interface IResponse {
+  Search: Array<IMovie>;
+  totalResults: number;
+  Response: "True";
+}
+
+interface IResponseError {
+  Response: "False";
+  Error: string;
+}
 
 export const getMovies = createAsyncThunk(
   "movie/getMovies",
-  (date, thunkApi) => {
+  async (data: IMovieSrcData, thunkApi) => {
     try {
-      const data = api.get("/");
-    } catch (error: unknown) {
-      //
+      const res: TResponse<IResponse | IResponseError> = await api.get(
+        `?apikey=${apiKey}&s=${data.src}`
+      );
+      switch (res.data.Response) {
+        case "True":
+          return thunkApi.fulfillWithValue(res.data.Search);
+        case "False":
+          throw new Error(res.data.Error);
+      }
+    } catch (err: unknown) {
+      const error = ensureError(err);
+      throw thunkApi.rejectWithValue(error.message);
     }
   }
 );
--- src/redux/slices/movie/movie.slice.ts
+++ src/redux/slices/movie/movie.slice.ts
@@ -1,21 +1,53 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { getMovies } from "./movie.er";
 import { TRootState } from "../../store";
+import { IMovie } from "../movie/movie.er";
+
+type TMovieSliceInitialState = {
+  movies: Array<IMovie>;
+  isFetching: boolean;
+  isError: boolean;
+  error: string;
+};
+
+const initialState: TMovieSliceInitialState = {
+  movies: [],
+  isFetching: false,
+  isError: false,
+  error: "",
+};
 
 const movieSlice = createSlice({
   name: "movies",
-  initialState: {
-    movies: [],
-    isFetching: false,
-    isError: false,
-    error: "",
-  },
+  initialState,
   reducers: {},
   extraReducers: (builder) => {
-    builder.addCase(getMovies.pending, (state, action) => {
-      // aksldjfl
-    });
+    builder
+      .addCase(getMovies.pending, (state) => {
+        state.isFetching = true;
+      })
+      .addCase(getMovies.fulfilled, (state, action) => {
+        console.log(`fulfilled-${JSON.stringify(action.payload)}`);
+
+        state.isFetching = false;
+        state.movies = action.payload;
+        state.isError = false;
+        state.error = "";
+      })
+      .addCase(getMovies.rejected, (state) => {
+        state.isFetching = false;
+        state.isError = true;
+        state.error = "";
+      });
   },
 });
 
 export default movieSlice;
+
+// #region : selectors
+
+export const selectAllMovies = (state: TRootState) => {
+  return state.movies.movies;
+};
+
+// #endregion : selectors
